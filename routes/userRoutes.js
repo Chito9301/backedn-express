673df53f5d2e6a9e41a992b3ebd8c593085ecd26
Chito9301 <ivanrba0193@gmail.com>
2025-09-02 01:18:55 -0400

    Add Routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+// backend/routes/userRoutes.js
+const express = require("express");
+const router = express.Router();
+const { getAllUsers, getUserById } = require("../controllers/userControllers");
+
+// Obtener todos los usuarios
+router.get("/", getAllUsers);
+
+// Obtener usuario por ID
+router.get("/:id", getUserById);
+
+module.exports = router;
